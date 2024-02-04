efcc5a899a4e786244225afc446f28872c25f4f4
epando07 <154563279+epando07@users.noreply.github.com>
2024-02-04 15:04:28 -0700

    Update page.js

--- page.js
+++ page.js
@@ -4,13 +4,33 @@ module.exports = {
     toField: '#to',
     phoneNumberField: '#phone',
     codeField: '#code',
+    cardNumber: '#number',
+    cardCode: '.card-second-row #code',
+    messageToTheDriver: '.#comment.input #Get some whiskey',
     // Buttons
     callATaxiButton: 'button=Call a taxi',
-    phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
+    phoneNumberButton: 'div*=phone number',
     nextButton: 'button=Next',
     confirmButton: 'button=Confirm',
+    paymentMethodButton: '.pp-text',
+    addCardButton: 'div=Add card',
+    linkCardButton: 'button=Link',
+    closePaymentMethodModalButton: '.payment-picker .close-button',
+    supportivePlanButton: '.tcard-icon',
+    messageToTheDriverField: 'div=.input-container',
+    orderRequirementsButton: 'div.reqs-header',
+    blanketAndHandkerchiefsButton: 'div.r-sw',
+    iceCreamButton: 'div=.r-counter-label',
+    iceCreamCounterButton: 'div-.counter',
+    iceCreamCounterPlusButton: 'div=.counter-plus',
+    placeOrderButton: '.smart-button-wrapper',
+
+
     // Modals
-    phoneNumberModal: '.modal',
+    PhoneNumberModal: '.modal',
+    // Misc
+    cardSignatureStrip: '.plc',
+    cardPaymentMethodIcon: 'img[alt="card"]',
     // Functions
     fillAddresses: async function(from, to) {
         const fromField = await $(this.fromField);
@@ -48,4 +68,36 @@ module.exports = {
         await codeField.setValue(code)
         await $(this.confirmButton).click()
     },
+    addPaymentMethodCard: async function() {
+        const paymentMethodButton = await $(this.paymentMethodButton)
+        await paymentMethodButton.waitForDisplayed();
+        await paymentMethodButton.click();
+
+        const addCardButton = await $(this.addCardButton);
+        await addCardButton.waitForDisplayed();
+        await addCardButton.click();
+
+        const cardNumber = await $(this.cardnumber);
+        await cardNumber.waitForDisplayed();
+        await cardNumber.setValue(1234567812345678);
+        const cardCode = await $(this.cardCode);
+        await cardCode.waitForDisplayed();
+        await cardCode.setValue(55);
+
+        const cardSignatureStrip = await $(this.cardSignatureStrip);
+        await cardSignatureStrip.waitForDisplayed();
+        await cardSignatureStrip.click();
+
+        const linkCardButton = await $(this.linkCardButton);
+        await linkCardButton.waitForDisplayed();
+        await linkCardButton.click();
+
+        const closePaymentMethodModalButton = await $(this.closePaymentMethodModalButton);
+        await closePaymentMethodModalButton.waitForDisplayed();
+        await closePaymentMethodModalButton.click();
+
+        const cardPaymentMethodIcon = await $(this.cardPaymentMethodIcon);
+        await cardPaymentMethodIcon.waitForDisplayed();
+        await expect(await $(cardPaymentMethodIcon)).toBeExisting();
+    }
 };
