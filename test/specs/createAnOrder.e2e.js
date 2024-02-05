e25e7a2cf4267866e181541c2543a26ae9d04402
epando07 <154563279+epando07@users.noreply.github.com>
2024-02-05 09:13:16 -0700

    Update createAnOrder.e2e.js

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -2,66 +2,65 @@ const page = require('../../page');
 const helper = require('../../helper');
 
 describe('Create an order', () => {
-    /* it('should open phone number modal', async () => {
-        await browser.url(`/`);
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumberButton = await $(page.phoneNumberButton);
-        await phoneNumberButton.waitForExist({ timeout: 20000 }); // Wait for the element to exist
-        await phoneNumberButton.click();
-        const phoneNumberModal = await $(page.phoneNumberModal);
-        await expect(phoneNumberModal).toBeExisting();
- 
-        
-    }); */
+    
 
     it('should wait for taxi driver', async () => {
         // Call the taxi to the address
         await browser.url(`/`);
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
     });
+    
 
         // Select supportive plan
-    describe('Selecting supportive plan', () => {
-        it('should select supportive plan', async () => {
+    it('should select supportive plan', async () => {
+        await browser.url(`/`);
         const supportivePlanButton = await $(page.supportivePlanButton);
         await supportivePlanButton.waitForDisplayed();
         await supportivePlanButton.click();
+
+        const supportivePlanButtonActive = await $(page.supportivePlanButtonActive);
+        await supportivePlanButtonActive.waitForDisplayed();
+        await expect (supportivePlanButtonActive).toBeExisting();
     });
+    
         
         
         // Input phone number
-    describe('Input phone number', () => {
-        it('should input phone number', async () => {
+    it('should input phone number', async () => {
+        await browser.url(`/`);
         const phoneNumber = helper.getPhoneNumber("+1");
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
     });
+    
         
 
         // Adding a payment card
-    describe('Add payment card', () => {
-        it('should add a payment card', async () => {
+    it('should add a payment card', async () => {
+        await browser.url(`/`);
         await page.addPaymentMethodCard();
 
         const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
         await cardPaymentMethodIcon.waitForDisplayed();
         await expect(await $(cardPaymentMethodIcon)).toBeExisting();
     });
+    
 
         // Leaving a message for the driver
-    describe('Leaving a message for the driver', () => {
-        it('should leave a message for the driver', async () => {
+    it('should leave a message for the driver', async () => {
+        await browser.url(`/`);
         const messageToTheDriverField = await $(page.messageToTheDriverField);
         await messageToTheDriverField.waitForDisplayed();
         await messageToTheDriverField.click();
         await messageToTheDriverField.setValue('Get some whiskey');
         await expect($(page.messageToTheDriver)).toHaveValue("Get some whiskey");
     });
+    
 
 
         // Ordering a blanket and handkerchiefs
-    describe('Ordering a blanket and handkerchiefs', () => {
-        it('should add a blanket and handkerchiefs', async () => {
+    it('should add a blanket and handkerchiefs', async () => {
+        await browser.url(`/`);
         const orderRequirementsButton = await $(page.orderRequirementsButton);
         await orderRequirementsButton.waitForDisplayed();
         await orderRequirementsButton.click();
@@ -69,11 +68,13 @@ describe('Create an order', () => {
         const blanketAndHandkerchiefsButton = await $(page.blanketAndHandkerchiefsButton);
         await blanketAndHandkerchiefsButton.waitForDisplayed();
         await blanketAndHandkerchiefsButton.click();
-    }):
+        await expect (blanketAndHandkerchiefsButton).toBeExisting();
+    });
+    
 
         // Ordering 2 ice creams
-    describe('Ordering ice cream', () => {
-        it('should add ice cream to the order', async () => {
+    it('should add ice cream to the order', async () => {
+        await browser.url(`/`);
         const iceCreamButton = await $(page.iceCreamButton);
         await iceCreamButton.waitForDisplayed();
         
@@ -84,12 +85,14 @@ describe('Create an order', () => {
         await iceCreamCounterPlusButton.setValue(2);
         await expect($(iceCreamCounterPlusButton)).toHaveValue(2);
     });
+    
 
         // The car search modal appears
-    describe('car search modal appears', () => {
-        it('should pop up car search modal', async () => {
+    it('should pop up car search modal', async () => {
+        await browser.url(`/`);
         const placeOrderButton = await $(page.placeOrderButton);
         await placeOrderButton.waitForDisplayed();
         await placeOrderButton.click();
+        await expect($(page.carSearchModal)).toBeExisting();
     });
 });
